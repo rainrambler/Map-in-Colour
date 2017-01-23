6357f0c14d62b614e8caf2aecef18d0ce735d588
Sublunarwind <kaidi.guo@outlook.com>
2017-01-23 16:09:56 +0100

    Fix fatal error on asking user location permission

--- app/src/main/java/com/example/kaidi/colourmap1/MapsActivity.java
+++ app/src/main/java/com/example/kaidi/colourmap1/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.kaidi.colourmap1;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,6 +8,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -33,12 +36,19 @@ import static com.example.kaidi.colourmap1.R.id.webview;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    final int LOCATION_REQUEST = 1340;
+    final String[] LOCATION_PERMS = {android.Manifest.permission.ACCESS_FINE_LOCATION};
+    LocationManager locationManager;
+    LocationListener locationListener;
+    Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//hide the original bar
+
+
 
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -53,59 +63,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         webView.loadUrl("file:///android_asset/map.html");
         //Here to set the leafletmap
 
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        final LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // log it when the location changes
-                if (location != null) {
-                    // Log.i("SuperMap", "Location changed : Lat: "
-                    //         + location.getLatitude() + " Lng: "
-                    //         + location.getLongitude());
-                }
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-        };
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                1000, 0, locationListener);
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        final double latitude = location.getLatitude();
-        final double longitude = location.getLongitude();
-
-        //final String position= "javascript:relocation("+longitude+","+latitude+")";
-        final String position= "javascript:relocation("+latitude+","+longitude+")";
-
-
-
-        final Button button = (Button) findViewById(R.id.locationbutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                webView.loadUrl(position);
-                //Toast.makeText(MapsActivity.this , position, Toast.LENGTH_LONG).show();
-                // Perform action on click
-            }
-        });
 
 
         Toolbar myToolbar = (Toolbar) findViewById(my_toolbar);
@@ -264,8 +221,91 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
 
 
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // log it when the location changes
+                if (location != null) {
+                    // Log.i("SuperMap", "Location changed : Lat: "
+                    //         + location.getLatitude() + " Lng: "
+                    //         + location.getLongitude());
+                }
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+        };
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, LOCATION_PERMS , 1340);
+
+            return;
+
+        }
+
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        final double latitude = location.getLatitude();
+                final double longitude = location.getLongitude();
+
+
+        final String position= "javascript:relocation("+latitude+","+longitude+")";
+
+
+        final Button button = (Button) findViewById(R.id.locationbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                webView.loadUrl(position);
+                //Toast.makeText(MapsActivity.this , position, Toast.LENGTH_LONG).show();
+                // Perform action on click
+            }
+        });
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
     }
+
+    private void reStartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case LOCATION_REQUEST:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 0, locationListener);
+                    onClickStartNewActivity();//First time start : show second view
+                } else {
+                    Toast.makeText(this, "Location cannot be obtained due to " + "missing permission.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+
+
+    }
+
 
 
     /**
@@ -282,9 +322,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     public void onClickStartNewActivity() {
